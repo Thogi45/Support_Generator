f744bfbfe11102217c47ed97fb13cce6b63d52f5
Thogi45 <44604151+Thogi45@users.noreply.github.com>
2020-03-28 18:24:46 +0100

    Test

--- Locate_STL.py
+++ Locate_STL.py
@@ -1,18 +1,40 @@
-import os,sys, glob
+import os,sys
 from stl import mesh, Mesh
+import glob
 
+path=os.path.dirname(sys.argv[0])
 
-LocateSTL=os.path.dirname(sys.argv[0])
-files = os.listdir(LocateSTL)
+files = os.listdir(path)
 i=1
 stllist=[]
 nbr=0
-while nbr==0:
-    for name in files:
-        if name.endswith(".stl")==True:
-            print(name," ....... ",i)
-            stllist.append(name)
-            i=i+1
+OK2=False
+while OK2==False:
+    i=1
+    LocateSTL1="{0}/*.stl".format(path)
+    Allfiles="{0}/*.*".format(path)
+    Allfiles2="{0}/.*".format(path)
+
+    Dir="{0}/*".format(path)
+    STL=glob.glob(LocateSTL1)
+    AllF=glob.glob(Allfiles)
+    AllF2=glob.glob(Allfiles2)
+
+    AllDF=glob.glob(Dir)
+    AllDF=AllDF+AllF2
+    AllF=AllF+AllF2
+    for k in range(len(AllF)):
+        AllDF.remove(AllF[k])
+    print("STL AVAILABLE IN THE FOLDER: ", path)
+    if len(STL)==0:
+        print("NO STL FILE, please go to another folder")
+    for name in STL:
+        print(name," ....... ",i)
+        i=i+1
+    print("NAVIGATION INTO FOLDERS")
+    for name in AllDF:
+        print("Go into folder: ",name," ....... ",i)
+        i=i+1
     print("Look back in another folder ....... ",0)
     typea=0
     OK=False
@@ -27,18 +49,32 @@ while nbr==0:
                     print("Enter a integer between ",0," and ",i-1)
              except:#Mais si l'utilisateur à rentrer autre chose que un entier alors on lui affiche "Veuillez entrer un nombre" et la boucle recommence.
                 print("Enter a integer between ",0," and ",i-1)
-        if nbr>=1:
-            print("You have selected: ", stllist[nbr-1])
-            STL=mesh.Mesh.from_file(stllist[nbr-1])
+        if nbr>=1 and nbr<=len(STL):
+            print("You have selected: ", STL[nbr-1])
+            STL1=mesh.Mesh.from_file(STL[nbr-1])
+        elif nbr>len(STL) and nbr<=len(STL)+len(AllDF):
+            print(nbr,len(STL))
+            path=AllDF[nbr-len(STL)-1]
+            files = os.listdir(path)
+            test=os.listdir(path)
+            print(files)
+            print(test)
         else:
             print("You have selected: Look in another folder")
+            path=os.path.dirname(path)
+            files = os.listdir(path)
+            test=os.listdir(path)
+            print(files)
+            print(test)
+
 
         Answer=input("Are you sure about your choice? YES/Y/y/yes/Yes: ")
         Yes=["YES","Y","y","yes","Yes"]
         if Answer not in Yes:
             OK=False
         else:
+            if nbr>=1 and nbr<=len(STL):
+                OK2=True
             OK=True
-    if nbr==0:
-        path=os.path.dirname(LocateSTL)
-        files = os.listdir(path)
+
+
